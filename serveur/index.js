2ebb68bf4a9c2bd6653bc13d77c6a91d7b462fe4
Guillaume Sissan <guillaume.sissan.pro@gmail.com>
2022-07-16 14:24:31 +0200

    Ajout de rate-limit

--- serveur/index.js
+++ serveur/index.js
@@ -9,15 +9,22 @@ const saucesRoutes = require("./routes/sauces");
 const path = require('path');
 const dotEnv = require("dotenv");
 dotEnv.config();
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 
-
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limiter chaque adresse IP à 100 requêtes toutes les 15mn
+    standardHeaders: true, // Renvoyer l'info de la limite atteinte aux `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 /**
  * permet de monter la fonction middleware spécifiée
  */
 app.use(express.json());
 app.use(cors());
+app.use(limiter);
 // Routes
 app.use("/api/auth", userRoutes);
 app.use("/api/sauces", saucesRoutes);
